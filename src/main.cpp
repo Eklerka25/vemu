753e8ba9e780a0c4ddec3138379cbbe4f41aeafc
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-05-04 14:18:21 +0200

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include <classes/cpu.hpp>
+#include <classes/memory.hpp>
+
+#include <typedefs.hpp>
+
+Cpu cpuInstance;
+
+int main() {
+    cpuInstance.Init();
+}
